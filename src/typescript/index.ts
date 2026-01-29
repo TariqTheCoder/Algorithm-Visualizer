0ce249402f536f0e20ffad8336f92e9d0dd90033
Tariq Tayebi <tariqtayebi1@outlook.com>
2026-01-30 01:34:57 +1100

    Set up

---
+++ src/typescript/index.ts
@@ -0,0 +1,6 @@
+console.log("Algorithm Visualizer is running!");
+
+// Example: simple array sorting visualizer logic
+const arr: number[] = [5, 2, 9, 1, 5, 6];
+arr.sort((a, b) => a - b);
+console.log("Sorted array:", arr);
